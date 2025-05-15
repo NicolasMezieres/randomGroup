2a3eae21a8feee1c7e72544909d4068bffa90e67
Daorse <mmorpgplays5@gmail.com>
2025-05-15 10:30:53 +0200

    Mentions légales HeaderDC/Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MentionsComponent } from "./pages/mentions/mentions.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MentionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
