e3db097008ced1b8c1ac80d05ad0d8634cdc81df
Daorse <mmorpgplays5@gmail.com>
2025-05-15 11:26:08 +0200

    Vitrine responsive footer/headerdc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MentionsComponent } from './pages/mentions/mentions.component';
 import { VitrineComponent } from './pages/vitrine/vitrine.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MentionsComponent, VitrineComponent],
+  imports: [RouterOutlet, VitrineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
