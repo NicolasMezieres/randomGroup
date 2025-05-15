4905b04e2c9f47b092e66c68ab7c200dea548879
Daorse <mmorpgplays5@gmail.com>
2025-05-15 10:48:14 +0200

    vitrine mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MentionsComponent } from "./pages/mentions/mentions.component";
+import { MentionsComponent } from './pages/mentions/mentions.component';
+import { VitrineComponent } from './pages/vitrine/vitrine.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MentionsComponent],
+  imports: [RouterOutlet, MentionsComponent, VitrineComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'randomGroup';
