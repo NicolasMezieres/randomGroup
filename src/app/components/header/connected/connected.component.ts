9bb78935f8100d602fca741601aeee16ee3ec316
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-05-15 11:04:21 +0200

    accueil and rectify header connected

--- src/app/components/header/connected/connected.component.ts
+++ src/app/components/header/connected/connected.component.ts
@@ -3,7 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-header-connected',
   imports: [MatIconModule],
-  templateUrl: './connected.component.html',
-  styleUrl: './connected.component.css',
+  template: `<header
+    class="bg-grey rounded-[20px] h-[50px] flex justify-center items-center gap-[50px] md:gap-[70px] mt-2.5 mx-[5px] shadow-[0_1px_4px_#21212170]"
+  >
+    <mat-icon aria-label="home icon" fontIcon="home"></mat-icon>
+    <mat-icon aria-label="profil icon" fontIcon="person"></mat-icon>
+    <mat-icon aria-label="logout icon" fontIcon="logout"></mat-icon>
+  </header> `,
 })
 export class ConnectedComponent {}
