1fdeb3abbd7f2c2fc5bee0db6bb27b15362560b1
Daorse <mmorpgplays5@gmail.com>
2025-05-15 11:38:04 +0200

    profils create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MentionsComponent } from "./pages/mentions/mentions.component";
+import { MentionsComponent } from './pages/mentions/mentions.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MentionsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'randomGroup';
