426115a76383857f8c3d732be339f6df16892c03
unknown <kyllianvibancos2@gmail.com>
2025-05-15 11:15:38 +0200

    ADDED : Login component, login path in app.routes.ts, Login HTML / CSS and Responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,23 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from "../app/app.component";
 import { RegisterComponent } from '../components/register/register.component';
+import { LoginComponent } from '../components/login/login.component';
 
 //Ajout des paths pour naviguer à register
 export const routes: Routes = [
-{
+  {
     path: '',
     title: 'Page Vitrine',
-    component: AppComponent
-}, {
+    component: AppComponent,
+  },
+  {
     path: 'register',
     title: 'Page Inscription',
-    component: RegisterComponent
-}
-
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    title: 'Page Connexion',
+    component: LoginComponent,
+  },
 ];
